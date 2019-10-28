6e22ddf9cbdcaa2c43d652dd2defe1eb70a26cbc
Lucas Dalbonio <dalbonio2@gmail.com>
2019-10-28 17:47:24 -0300

    adjusting columns

--- src/entities/aluno.entity.ts
+++ src/entities/aluno.entity.ts
@@ -1,7 +1,8 @@
-import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany, Unique } from "typeorm";
 import { Endereco } from './endereco.entity';
 
 @Entity()
+@Unique(['cpf'])
 export class Aluno{
     @PrimaryGeneratedColumn()
     id: number
--- src/entities/endereco.entity.ts
+++ src/entities/endereco.entity.ts
@@ -1,4 +1,4 @@
-import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn } from "typeorm";
 import { Aluno } from './aluno.entity'
 
 @Entity()
@@ -19,5 +19,6 @@ export class Endereco{
     bairro: string
 
     @ManyToOne(type => Aluno, aluno => aluno.enderecos)
+    @JoinColumn({name: "aluno_id"})
     aluno: Aluno
 }

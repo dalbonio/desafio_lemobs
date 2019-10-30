eebd1bcc1150a4a7b9a17976987532a7066c9d58
Lucas Dalbonio <dalbonio2@gmail.com>
2019-10-30 19:15:50 -0300

    adding swagger params in requests

--- src/controllers/aluno.controller.ts
+++ src/controllers/aluno.controller.ts
@@ -3,6 +3,7 @@ import { AlunoService } from '../services/aluno.service';
 import { EnderecoService } from '../services/endereco.service';
 import { tsBooleanKeyword, numberLiteralTypeAnnotation } from '@babel/types';
 import { Utils, testaCPF } from '../utils/utils';
+import { ApiImplicitBody, ApiImplicitParam } from '@nestjs/swagger';
 
 @Controller('aluno')
 export class AlunoController {
@@ -23,6 +24,8 @@ export class AlunoController {
   //retornar os dados de todos os alunos que possuem nota maior que a nota dada como parâmetro se o
   //criterio for igual a “>” e os que possuem nota menor se o criterio for igual a “<”;
   @Get(":nota/criterio/:criterio")
+  @ApiImplicitParam({ name: 'nota' })
+  @ApiImplicitParam({ name: 'criterio' })
   async getAlunoByCriterio(@Param() params): Promise<{}> {
     const nota = params.nota
     let criterio = params.criterio
@@ -32,6 +35,7 @@ export class AlunoController {
 
   //retornar dados de um único aluno
   @Get(":id")
+  @ApiImplicitParam({ name: 'id' })
   async getOneAluno(@Param('id') id): Promise<{}> {
     console.log(id)
     return await this.alunoService.getAlunoById(id)
@@ -39,6 +43,7 @@ export class AlunoController {
 
   //retornar todos os endereços de um aluno e sua quantidade;
   @Get(":id/endereco")
+  @ApiImplicitParam({ name: 'id' })
   async getAlunoAndEndereco(@Param('id') id): Promise<{}> {
     const enderecos = await this.alunoService.getEnderecosOfAluno(id)
     const formattedEnderecos = this.enderecoService.formatEnderecos(enderecos)
--- src/controllers/endereco.controller.ts
+++ src/controllers/endereco.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get, Post, Body, Put, Param, Query } from '@nestjs/common';
 import { EnderecoService } from '../services/endereco.service';
+import { ApiImplicitQuery } from '@nestjs/swagger';
 
 @Controller('endereco')
 export class EnderecoController {
@@ -7,8 +8,13 @@ export class EnderecoController {
 
   //retornar dados de todos os endereços;
   @Get()
+  @ApiImplicitQuery({
+    name: 'bairro',
+    required: false,
+    type: String
+  })
   getEnderecos(@Query("bairro") bairro: string): Promise<{}> {
-    return this.enderecoService.getAllEnderecos(bairro);
+    return this.enderecoService.getAllEnderecos(bairro); 
   }
 
   //cadastrar um endereco
